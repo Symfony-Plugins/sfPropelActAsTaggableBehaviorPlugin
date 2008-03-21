0199af2e73fbf291dfe268dec7f5dd5b7c2a4daf
xavier <xavier@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-03-21 06:55:47 -0500

    [wiki:sfPropelActAsTaggableBehaviorPlugin]: made plugin compatible with Symfony 1.1 (thanks to Nicolas Perriault)
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelActAsTaggableBehaviorPlugin/trunk@8022 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfPropelActAsTaggableBehavior.class.php
+++ lib/sfPropelActAsTaggableBehavior.class.php
@@ -1,46 +1,46 @@
 <?php
 /*
  * This file is part of the sfPropelActAsTaggableBehavior package.
- * 
+ *
  * (c) 2007 Xavier Lacot <xavier@lacot.org>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
 /*
- * This behavior permits to attach tags to Propel objects. Some more bits about 
+ * This behavior permits to attach tags to Propel objects. Some more bits about
  * the philosophy of the stuff:
- * 
+ *
  * - taggable objects must have a primary key
- * 
+ *
  * - tags are saved when the object is saved, not before
- * 
+ *
  * - one object cannot be tagged twice with the same tag. When trying to use
  *   twice the same tag on one object, the second tagging will be ignored
- * 
- * - the tags associated to one taggable object are only loaded when necessary. 
+ *
+ * - the tags associated to one taggable object are only loaded when necessary.
  *   Then they are cached.
- * 
- * - once created, tags never change in the Tag table. When using replaceTag(), 
+ *
+ * - once created, tags never change in the Tag table. When using replaceTag(),
  *   a new tag is created id necessary, but the old one is not deleted.
- * 
- * 
+ *
+ *
  * The plugin associates a parameterHolder to Propel objects, with 3 namespaces:
- * 
+ *
  * - tags:
  *     Tags that have been attached to the object, but not yet saved.
  *     Contract: tags are disjoin of (saved_tags union removed_tags)
- * 
+ *
  * - saved_tags:
  *     Tags that are presently saved in the database
- * 
+ *
  * - removed_tags:
- *     Tags that are presently saved in the database, but which will be removed 
+ *     Tags that are presently saved in the database, but which will be removed
  *     at the next save()
  *     Contract: removed_tags are disjoin of (tags union saved_tags)
- * 
- * 
+ *
+ *
  * @author   Xavier Lacot <xavier@lacot.org>
  * @see      http://www.symfony-project.com/trac/wiki/sfPropelActAsTaggableBehaviorPlugin
  */
@@ -54,7 +54,18 @@ class sfPropelActAsTaggableBehavior
   {
     if ((!isset($object->_tags)) || ($object->_tags == null))
     {
-      $object->_tags = new sfParameterHolder();
+      if (class_exists('sfNamespacedParameterHolder'))
+      {
+        // Symfony 1.1
+        $parameter_holder = 'sfNamespacedParameterHolder';
+      }
+      else
+      {
+        // Symfony 1.0
+        $parameter_holder = 'sfParameterHolder';
+      }
+
+      $object->_tags = new $parameter_holder();
     }
 
     return $object->_tags;
@@ -69,7 +80,7 @@ class sfPropelActAsTaggableBehavior
       self::getTagsHolder($object)->set($tag, $tag, 'tags');
     }
   }
-  
+
   private static function clear_tags(BaseObject $object)
   {
     return self::getTagsHolder($object)->removeNamespace('tags');
@@ -85,12 +96,12 @@ class sfPropelActAsTaggableBehavior
     self::clear_tags($object);
     self::getTagsHolder($object)->add($tags, 'tags');
   }
-  
+
   private static function add_saved_tag(BaseObject $object, $tag)
   {
     self::getTagsHolder($object)->set($tag, $tag, 'saved_tags');
   }
-  
+
   private static function clear_saved_tags(BaseObject $object)
   {
     return self::getTagsHolder($object)->removeNamespace('saved_tags');
@@ -111,7 +122,7 @@ class sfPropelActAsTaggableBehavior
   {
     self::getTagsHolder($object)->set($tag, $tag, 'removed_tags');
   }
-  
+
   private static function clear_removed_tags(BaseObject $object)
   {
     return self::getTagsHolder($object)->removeNamespace('removed_tags');
@@ -130,15 +141,15 @@ class sfPropelActAsTaggableBehavior
 
 
   /**
-   * Adds a tag to the object. The "tagname" param can be a string or an array 
+   * Adds a tag to the object. The "tagname" param can be a string or an array
    * of strings. These 3 code sequences produce an equivalent result :
-   * 
+   *
    * 1- $object->addTag('tag1,tag2,tag3');
    * 2- $object->addTag('tag1');
    *    $object->addTag('tag2');
    *    $object->addTag('tag3');
    * 3- $object->addTag(array('tag1','tag2','tag3'));
-   * 
+   *
    * @param      BaseObject  $object
    * @param      mixed       $tagname
    */
@@ -178,7 +189,7 @@ class sfPropelActAsTaggableBehavior
   /**
    * Retrieves from the database tags that have been atached to the object.
    * Once loaded, this saved tags list is cached and updated in memory.
-   * 
+   *
    * @param      BaseObject  $object
    */
   private function getSavedTags(BaseObject $object)
@@ -207,9 +218,9 @@ class sfPropelActAsTaggableBehavior
   }
 
   /**
-   * Returns the list of the tags attached to the object, whatever they have 
+   * Returns the list of the tags attached to the object, whatever they have
    * already been saved or not.
-   * 
+   *
    * @param      BaseObject  $object
    */
   public function getTags(BaseObject $object, $options = array())
@@ -226,16 +237,16 @@ class sfPropelActAsTaggableBehavior
   }
 
   /**
-   * Returns true if the object has a tag. If a tag ar an array of tags is 
+   * Returns true if the object has a tag. If a tag ar an array of tags is
    * passed in second parameter, checks if these tags are attached to the object
-   * 
+   *
    * These 3 calls are equivalent :
-   * 1- $object->hasTag('tag1') 
-   *    && $object->hasTag('tag2') 
+   * 1- $object->hasTag('tag1')
+   *    && $object->hasTag('tag2')
    *    && $object->hasTag('tag3');
    * 2- $object->hasTag('tag1,tag2,tag3');
    * 3- $object->hasTag(array('tag1', 'tag2', 'tag3'));
-   * 
+   *
    * @param      BaseObject  $object
    * @param      mixed       $tag
    */
@@ -260,7 +271,7 @@ class sfPropelActAsTaggableBehavior
 
       if ($tag === null)
       {
-        return (count($tags) > 0) || (count($this->getSavedTags($object)) > 0); 
+        return (count($tags) > 0) || (count($this->getSavedTags($object)) > 0);
       }
       elseif (is_string($tag))
       {
@@ -286,10 +297,10 @@ class sfPropelActAsTaggableBehavior
   }
 
   /**
-   * Preload tags for a set of objects. It might be usefull in case you want to 
-   * display a long list of taggable objects with their associated tags: it 
+   * Preload tags for a set of objects. It might be usefull in case you want to
+   * display a long list of taggable objects with their associated tags: it
    * avoids to load tags per object, and gets all tags in a few requests.
-   * 
+   *
    * @param      array       $objects
    */
   public static function preloadTags(&$objects)
@@ -357,7 +368,7 @@ class sfPropelActAsTaggableBehavior
 
   /**
    * Tags saving logic, runned after the object himself has been saved
-   * 
+   *
    * @param      BaseObject  $object
    */
   public function postSave(BaseObject $object)
@@ -402,7 +413,7 @@ class sfPropelActAsTaggableBehavior
 
   /**
    * Removes all the tags associated to the object.
-   * 
+   *
    * @param      BaseObject  $object
    */
   public function removeAllTags(BaseObject $object)
@@ -415,9 +426,9 @@ class sfPropelActAsTaggableBehavior
   }
 
   /**
-   * Removes a tag or a set of tags from the object. As usual, the second 
+   * Removes a tag or a set of tags from the object. As usual, the second
    * parameter might be an array of tags or a comma-separated string.
-   * 
+   *
    * @param      BaseObject  $object
    * @param      mixed       $tagname
    */
@@ -454,9 +465,9 @@ class sfPropelActAsTaggableBehavior
   }
 
   /**
-   * Replaces a tag with an other one. If the third optionnal parameter is not 
+   * Replaces a tag with an other one. If the third optionnal parameter is not
    * passed, the second tag will simply be removed
-   * 
+   *
    * @param      BaseObject  $object
    * @param      String      $tagname
    * @param      String      $replacement
